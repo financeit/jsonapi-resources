93cea3a208c8353c3265bf0dbf0607ce1a616bcc
Andrew Szczepanski <aszczepanski@financeit.io>
2024-08-27 09:00:57 -0400

    Get tests to pass for Ruby 3.1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -80,7 +80,7 @@ end
 # Monkeypatch ActionController::TestCase to delete the RAW_POST_DATA on subsequent calls in the same test.
 if Rails::VERSION::MAJOR >= 5
   module ClearRawPostHeader
-    def process(action, *args)
+    def process(action, **args)
       @request.delete_header 'RAW_POST_DATA'
       super
     end
@@ -95,7 +95,7 @@ end
 # in a standard way.
 if Rails::VERSION::MAJOR < 5
   module Rails4ActionControllerProcess
-    def process(*args)
+    def process(**args)
       if args[2] && args[2][:params]
         args[2] = args[2][:params]
       end
@@ -519,14 +519,15 @@ class ActionDispatch::IntegrationTest
 end
 
 class ActionController::TestCase
-  def assert_cacheable_get(action, *args)
+  def assert_cacheable_get(action, **args)
     assert_nil JSONAPI.configuration.resource_cache
 
     normal_queries = []
     normal_query_callback = lambda {|_, _, _, _, payload| normal_queries.push payload[:sql] }
     ActiveSupport::Notifications.subscribed(normal_query_callback, 'sql.active_record') do
-      get action, *args
+      get action, **args
     end
+
     non_caching_response = json_response_sans_backtraces
     non_caching_status = response.status
 
@@ -559,7 +560,7 @@ class ActionController::TestCase
               @controller = nil
               setup_controller_request_and_response
               @request.headers.merge!(orig_request_headers.dup)
-              get action, *args
+              get action, **args
             end
           end
         rescue Exception
